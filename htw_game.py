0bc14be606eac6b912defcbbada7f070f41a8ec7
Jesse <jessedsimpson@hotmail.com>
2015-11-09 08:36:57 -0600

    Added comments explaining what was done so far

--- htw_game.py
+++ htw_game.py
@@ -3,7 +3,21 @@
 #---------------------------------------------------------------------
 
 import random
-# Variable locations according to dodecahedron picture in files
+
+# Class for Caves
+class Cave:
+    # Constructor sets the value(location of cave to 0 or NULL)
+    def __init__(self):
+        self.__value = 0
+    # Function to set Cave location to value
+    def set_value(self, number):
+        self.__value = number
+    # Return value as in "You are in Cave: " Cave.get_value()
+    def get_value(self):
+        return self.__value
+
+
+# Variable locations according to dodecahedron picture in file
 POINT1 = 1
 POINT2 = 2
 POINT3 = 3
@@ -24,16 +38,8 @@ POINT17 = 17
 POINT18 = 18
 POINT19 = 19
 POINT20 = 20
-class Cave:
-    def __init__(self):
-        self.__value = 0
 
-    def set_value(self, number):
-        self.__value = number
-
-    def get_value(self):
-        return self.__value
-# Cave Variables initialized to 0
+# Cave Variables objects Initialized
 Cave1 = Cave()
 Cave2 = Cave()
 Cave3 = Cave()
@@ -55,18 +61,22 @@ Cave18 = Cave()
 Cave19 = Cave()
 Cave20 = Cave()
 
+# List containing all the Locations for caves
 point_list = [POINT1, POINT2, POINT3, POINT4, POINT5, POINT6, POINT7, POINT8,
               POINT9, POINT10, POINT11, POINT12, POINT13, POINT14, POINT15,
               POINT16, POINT17, POINT18, POINT19, POINT20]
 
+# List containg all the Cave Objects
 cave_list = [Cave1, Cave2, Cave3, Cave4, Cave5, Cave6, Cave7, Cave8, Cave9,
              Cave10, Cave11, Cave12, Cave13, Cave14, Cave15, Cave16, Cave17,
              Cave18, Cave19, Cave20]
 
+# Loop to go through cave_list and assign it a random value from
+# the point_list, then removes that value from the point_list
 for i in range(0, 20, 1):
     cave_list[i].set_value(random.choice(point_list))
     point_list.remove(cave_list[i].get_value())
 
-
+# Test print to see Cave Objects Values/(other variabes in the future)
 for i in range(0, 20, 1):
-    print("Cave", i, "Value:", cave_list[i].get_value())
+    print("Cave", i + 1, "Value:", cave_list[i].get_value())
