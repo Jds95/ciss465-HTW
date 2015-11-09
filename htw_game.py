31e9702529e3a8eb237284af497ba9b9fe4f2c7f
Jesse <jessedsimpson@hotmail.com>
2015-11-09 08:26:22 -0600

    Updated Cave to Class, added picture of map for game to follow via point values

--- htw_game.py
+++ htw_game.py
@@ -24,27 +24,36 @@ POINT17 = 17
 POINT18 = 18
 POINT19 = 19
 POINT20 = 20
+class Cave:
+    def __init__(self):
+        self.__value = 0
+
+    def set_value(self, number):
+        self.__value = number
+
+    def get_value(self):
+        return self.__value
 # Cave Variables initialized to 0
-Cave1 = 0
-Cave2 = 0
-Cave3 = 0
-Cave4 = 0
-Cave5 = 0
-Cave6 = 0
-Cave7 = 0
-Cave8 = 0
-Cave9 = 0
-Cave10 = 0
-Cave11 = 0
-Cave12 = 0
-Cave13 = 0
-Cave14 = 0
-Cave15 = 0
-Cave16 = 0
-Cave17 = 0
-Cave18 = 0
-Cave19 = 0
-Cave20 = 0
+Cave1 = Cave()
+Cave2 = Cave()
+Cave3 = Cave()
+Cave4 = Cave()
+Cave5 = Cave()
+Cave6 = Cave()
+Cave7 = Cave()
+Cave8 = Cave()
+Cave9 = Cave()
+Cave10 = Cave()
+Cave11 = Cave()
+Cave12 = Cave()
+Cave13 = Cave()
+Cave14 = Cave()
+Cave15 = Cave()
+Cave16 = Cave()
+Cave17 = Cave()
+Cave18 = Cave()
+Cave19 = Cave()
+Cave20 = Cave()
 
 point_list = [POINT1, POINT2, POINT3, POINT4, POINT5, POINT6, POINT7, POINT8,
               POINT9, POINT10, POINT11, POINT12, POINT13, POINT14, POINT15,
@@ -55,9 +64,9 @@ cave_list = [Cave1, Cave2, Cave3, Cave4, Cave5, Cave6, Cave7, Cave8, Cave9,
              Cave18, Cave19, Cave20]
 
 for i in range(0, 20, 1):
-    cave_list[i] = random.choice(point_list)
-    print("Cave", i, "Value: ", cave_list[i])
-    point_list.remove(cave_list[i])
+    cave_list[i].set_value(random.choice(point_list))
+    point_list.remove(cave_list[i].get_value())
 
-print(cave_list)
-print(point_list)
+
+for i in range(0, 20, 1):
+    print("Cave", i, "Value:", cave_list[i].get_value())
