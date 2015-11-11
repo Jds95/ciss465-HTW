f629c1bb05695f011278a36ec816705071d22b48
Jesse <jessedsimpson@hotmail.com>
2015-11-11 15:54:50 -0600

    Working connections for caves

--- Cave.py
+++ Cave.py
@@ -4,17 +4,21 @@ class Cave:
     def __init__(self):
         self.__value = 0
         self.__connection =[]
+        self.__has_wumpus = False
+        self.__has_pit = False
+        self.__has_bat = False
+        
     # Function to set Cave location to value
     def set_value(self, number):
         self.__value = number
+        
     # Return value as in "You are in Cave: " Cave.get_value()
     def get_value(self):
         return self.__value
     
     # Set the connections between the rooms via chart
-    def set_connection(self, list):
-        for item in list:
-            self.__connection.append(item)
+    def add_connection(self, item):
+        self.__connection.append(item)
             
     # Return the list of what it connects to
     def get_connection(self):
@@ -22,11 +26,25 @@ class Cave:
 
     # Bool check for list to see if it has 3 items for connections
     def is_connection_full(self):
-        if (len(self.__connection) < 3):
-            return False
-        else:
+        if (len(self.__connection) == 3):
             return True
-
+        elif (len(self.__connection) < 3):
+            return False
+    
+    # Bool check for list to see if it has 2 items for connections
+    def two_connect(self):
+        if (len(self.__connection) == 2):
+            return True
+        else:
+            return False
+        
+    # Bool check for list to see if it has no connections
+    def is_connection_empty(self):
+        if (len(self.__connection) == 0):
+            return True
+        else:
+            return False
+        
     # Default print statement, can add booleans in later
     def __str__(self):
         rep = "Cave: "
--- htw_game.py
+++ htw_game.py
@@ -7,27 +7,52 @@ CAVE_NUMBERS = 20
 import random
 import Cave
 
-# List containing all the Locations for caves
-point_list = []
+def cave_generation(cave_list, cave_list_copy):
+    
+    # List containing all the Locations for caves
+    point_list = []
 
-for i in range (CAVE_NUMBERS):
-    point_list.append(i + 1)
+    for i in range (CAVE_NUMBERS):
+        point_list.append(i + 1)
 
+    # Loop to add caves to cave_list
+    for i in range (CAVE_NUMBERS):
+        cave = Cave.Cave()
+        cave_list.append(cave)
+        
+    # Loop to go through cave_list and assign it a random value from
+    # the point_list, then removes that value from the point_list
+    for i in range (CAVE_NUMBERS):
+        cave_list[i].set_value(random.choice(point_list))
+        point_list.remove(cave_list[i].get_value())
+        
+    cave_list_copy = cave_list[:]
+            
+    # Set up connections for the caves
+    for i in range(3):
+        for item in cave_list:
+            if (item not in cave_list_copy):
+                continue
+            item_copy = item
+            if (item in cave_list_copy):
+                cave_list_copy.remove(item)
+                random_cave = random.choice(cave_list_copy)
+                item_copy.add_connection(random_cave.get_value())
+                random_cave.add_connection(item_copy.get_value())
+                if (random_cave in cave_list_copy):
+                    cave_list_copy.remove(random_cave)
+
+        cave_list_copy = cave_list[:]
+            
+   
 # List containing all the Caves
 cave_list = []
+cave_check = []
+cave_list_copy = []
 
-# Loop to add caves to cave_list
-for i in range (CAVE_NUMBERS):
-    cave = Cave.Cave()
-    cave_list.append(cave)
+cave_generation(cave_list, cave_list_copy)
 
-# Loop to go through cave_list and assign it a random value from
-# the point_list, then removes that value from the point_list
-for i in range (CAVE_NUMBERS):
-    cave_list[i].set_value(random.choice(point_list))
-    point_list.remove(cave_list[i].get_value())
-
+print("Cave List Print")
 # Test print to see Cave Objects Values/(other variabes in the future)
 for item in cave_list:
     print(item)
-    
